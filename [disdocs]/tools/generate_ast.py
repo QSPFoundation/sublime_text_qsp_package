201e8ee70fcce33ab99741a885d32b69c9e615ea
aleksversus <aleksversus@mail.ru>
2025-09-17 17:01:20 +0500

    feat(aux-tools): generator ast

---
+++ [disdocs]/tools/generate_ast.py
@@ -0,0 +1,85 @@
+import sys, os
+from typing import List, Dict
+
+class GenerateAst:
+
+    def __init__(self, args:List[str]) -> None:
+        if len(args) != 1:
+            print("Usage: generate_ast <output directory>")
+            sys.exit(64)
+        self.output_folder:str = args[0]
+
+        arr = {
+            "QspBinary"   : "left:QspExpr, operator:QspToken, right:QspExpr",
+            "QspGrouping" : "expression:QspExpr",
+            "QspLiteral"  : "value:Any",
+            "QspUnary"    : "operator:QspToken, right:QspToken"
+        }
+
+        self.define_ast("QspExpr", arr)
+
+    def define_ast(self, base_name:str, types:Dict[str, str]) -> None:
+        path = os.path.join(self.output_folder, f'{base_name.lower()}.py')
+        with open(path, 'w', encoding='utf-8') as fp:
+            lines:List[str] = [
+                'from abc import ABC, abstractmethod',
+                'from dataclasses import dataclass',
+                'from typing import Generic, TypeVar, Any',
+                'R = TypeVar("R")',
+                
+                f'class {base_name}(ABC, Generic[R]):',
+                f'    """Класс поддержки выражений. Используется в т.ч. для указания типов."""',
+                f'    @abstractmethod',
+                f'    def accept(self, visitor: "Visitor[R]") -> R:',
+                f'        ...'
+            ]
+
+            self.define_visitor(lines, base_name, types)
+
+            for class_name, fields in types.items():
+                self.define_type(lines, base_name, class_name, fields)
+            
+            fp.write('\n'.join(lines))
+
+    def define_visitor(self, lines:List[str], base_name:str, types:Dict[str, str]) -> None:
+        b = base_name[3:].lower()
+        lines.extend(['class Visitor(ABC, Generic[R]):',
+            '    """interface of visitor for Expression"""'
+        ])
+        for type_name in types.keys():
+            t = type_name[3:].lower()
+            lines.extend([
+                f'    @abstractmethod',
+                f'    def visit_{t}_{b}(self, expr:R) -> R:',
+                f'        ...',
+                f''
+            ])
+
+    def define_type(self, lines:List[str], base_name:str, class_name:str, fields:str) -> None:
+        c = class_name[3:].lower()
+        b = base_name[3:].lower()
+        lines.extend([
+            '\n@dataclass',
+            f'class {class_name}({base_name}):',
+           
+        ])
+        # constructor and 
+        # store parameters in fields
+        field_list:List[str] = fields.split(', ')
+        for field in field_list:
+            name, ext = field.split(":")
+            lines.append(f'    {name}: {ext}')
+        # fields initialisation in py not needs
+        # ...
+        # visitor pattern
+        lines.extend([
+            f'    def accept(self, visitor: Visitor[R]) -> R:',
+            f'        return visitor.visit_{c}_{b}(self)\n'
+        ])
+
+
+def main():
+    gen = GenerateAst('.')
+
+if __name__ == "__main__":
+    main()
---
+++ [disdocs]/tools/qspexpr.py
@@ -0,0 +1,57 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Generic, TypeVar, Any
+R = TypeVar("R")
+class QspExpr(ABC, Generic[R]):
+    """Класс поддержки выражений. Используется в т.ч. для указания типов."""
+    @abstractmethod
+    def accept(self, visitor: "Visitor[R]") -> R:
+        ...
+class Visitor(ABC, Generic[R]):
+    """interface of visitor for Expression"""
+    @abstractmethod
+    def visit_binary_expr(self, expr:R) -> R:
+        ...
+
+    @abstractmethod
+    def visit_grouping_expr(self, expr:R) -> R:
+        ...
+
+    @abstractmethod
+    def visit_literal_expr(self, expr:R) -> R:
+        ...
+
+    @abstractmethod
+    def visit_unary_expr(self, expr:R) -> R:
+        ...
+
+
+@dataclass
+class QspBinary(QspExpr):
+    left: QspExpr
+    operator: QspToken
+    right: QspExpr
+    def accept(self, visitor: Visitor[R]) -> R:
+        return visitor.visit_binary_expr(self)
+
+
+@dataclass
+class QspGrouping(QspExpr):
+    expression: QspExpr
+    def accept(self, visitor: Visitor[R]) -> R:
+        return visitor.visit_grouping_expr(self)
+
+
+@dataclass
+class QspLiteral(QspExpr):
+    value: Any
+    def accept(self, visitor: Visitor[R]) -> R:
+        return visitor.visit_literal_expr(self)
+
+
+@dataclass
+class QspUnary(QspExpr):
+    operator: QspToken
+    right: QspToken
+    def accept(self, visitor: Visitor[R]) -> R:
+        return visitor.visit_unary_expr(self)
