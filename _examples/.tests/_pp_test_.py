c506df36baf9f94251dcb7dd06d37ce0c1e73cb8
aleksversus <aleksversus@mail.ru>
2026-04-19 22:26:11 +0500

    test replaces

--- _examples/.tests/_pp_test_.py
+++ _examples/.tests/_pp_test_.py
@@ -1,20 +1,27 @@
-# _pp_test_.py
+# QSP.sublime-package/_pp_test_.py
+import json, os
+from typing import Dict, List
 
 if __name__ == "__main__":
-    from pp_ast_printer import AstPrinter
-    from dirs_ast_printer import DirsAstPrinter
-    pathes:Dict[str, str] = {
-        'input': '..\\..\\..\\_examples\\example_preprocessor\\pptest.qsps',
-        'output': '..\\..\\..\\_examples\\example_preprocessor\\_test\\output.qsps',
-        'expected': '..\\..\\..\\_examples\\example_preprocessor\\expected.qsps',
+    os.chdir(os.path.dirname(__file__))
 
-        'dirs_tokens': '..\\..\\..\\_examples\\example_preprocessor\\_test\\01_dirs_tokens.json',
-        'dirs_tree': '..\\..\\..\\_examples\\example_preprocessor\\_test\\02_dirs_tree.json',
-        'dirs_lines': '..\\..\\..\\_examples\\example_preprocessor\\_test\\03_dirs_ml.json',
-        
-        'pp_tokens': '..\\..\\..\\_examples\\example_preprocessor\\_test\\04_pp_tokens.json',
-        'pp_tree': '..\\..\\..\\_examples\\example_preprocessor\\_test\\05_pp_ast.json',
-        'pp_lines': '..\\..\\..\\_examples\\example_preprocessor\\_test\\06_pp_fast_out.json'
+Path = str
+
+if __name__ == "__main__":
+    from qSpy.preprocessor import AstPrinter, DirsAstPrinter, QspsPP
+
+    pathes:Dict[str, str] = {
+        'input': '..\\_examples\\example_preprocessor\\pptest.qsps',
+        'output': '..\\_examples\\example_preprocessor\\_test\\output.qsps',
+        'expected': '..\\_examples\\example_preprocessor\\expected.qsps',
+
+        'dirs_tokens': '..\\_examples\\example_preprocessor\\_test\\01_dirs_tokens.json',
+        'dirs_tree': '..\\_examples\\example_preprocessor\\_test\\02_dirs_tree.json',
+        'dirs_lines': '..\\_examples\\example_preprocessor\\_test\\03_dirs_ml.json',
+
+        'pp_tokens': '..\\_examples\\example_preprocessor\\_test\\04_pp_tokens.json',
+        'pp_tree': '..\\_examples\\example_preprocessor\\_test\\05_pp_ast.json',
+        'pp_lines': '..\\_examples\\example_preprocessor\\_test\\06_pp_fast_out.json'
     }
 
     def _read_file(path:Path) -> List[str]:
@@ -31,7 +38,7 @@ if __name__ == "__main__":
             json.dump(obj, fp, ensure_ascii=False, indent=2)
 
     qsps_lines = _read_file(pathes['input'])
-    
+
     import time
     old = time.time()
     preprocessor = QspsPP('On')
@@ -59,7 +66,7 @@ if __name__ == "__main__":
     _write_json(pathes['pp_lines'], preprocessor.pp_fastlines())
 
     expected_lines = _read_file(pathes['expected'])
-    
+
     if len(expected_lines) == len(new_lines):
         stop = False
         for i, line in enumerate(expected_lines):
